c6b961d681da0619e6f9977d3df99a8c8785d7ca
Martijn van de Bovenkamp <45668793+bovenkam@users.noreply.github.com>
2022-07-22 15:33:59 +0100

    Remove endpoint

--- Braze-iOS-SDK/AppDelegate.swift
+++ Braze-iOS-SDK/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let apiKey = "<change to your key>"
-        let endpointToUse = "sondheim.braze.com"
+        let endpointToUse = "<change to endpoint to use>"
         
         var appboyOptions: [String: Any] {
           return [
